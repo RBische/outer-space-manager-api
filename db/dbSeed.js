ab56a75f36157fc2e0caf52c97de08cdcf5d51c0
Raphaël Bischof <raphael@in-and-win.com>
2016-12-20 00:12:37 +0100

    feat: Added some dummy data for buildings

--- db/dbSeed.js
+++ db/dbSeed.js
@@ -4,15 +4,55 @@ var seed = {
     var admin = require('../db/db');
     // As an admin, the app has access to read and write all data, regardless of Security Rules
     var db = admin.database();
-    var ref = db.ref("outer-space-manager").child("buildings");
-    ref.remove();
-    ref.update(
+    var refBuildings = db.ref("outer-space-manager").child("buildings");
+    refBuildings.remove();
+    refBuildings.update(
     {
       0:{
-        "name": "Centrale électrique"
+        "name": "Centrale électrique",
+        "effect":"speed_building",
+        "amountOfEffectByLevel":100,
+        "amountOfEffectLevel0":100,
+        "timeToBuildByLevel":200,
+        "timeToBuildLevel0":60,
+        "costByLevel":200,
+        "costLevel0":100
       },
       1:{
-        "name": "Centrale électrique"
+        "name": "Spatioport",
+        "effect":"speed_fleet",
+        "amountOfEffectByLevel":100,
+        "amountOfEffectLevel0":100,
+        "timeToBuildByLevel":200,
+        "timeToBuildLevel0":60,
+        "costByLevel":200,
+        "costLevel0":100
+      }
+    }
+    );
+    var refVessels = db.ref("outer-space-manager").child("vessels");
+    refVessels.remove();
+    refVessels.update(
+    {
+      0:{
+        "name": "Centrale électrique",
+        "effect":"speed_building",
+        "amountOfEffectByLevel":100,
+        "amountOfEffectLevel0":100,
+        "timeToBuildByLevel":200,
+        "timeToBuildLevel0":60,
+        "costByLevel":200,
+        "costLevel0":100
+      },
+      1:{
+        "name": "Spatioport",
+        "effect":"speed_fleet",
+        "amountOfEffectByLevel":100,
+        "amountOfEffectLevel0":100,
+        "timeToBuildByLevel":200,
+        "timeToBuildLevel0":60,
+        "costByLevel":200,
+        "costLevel0":100
       }
     }
     );
